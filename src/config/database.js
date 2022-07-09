5406a444b60f9d019d924d051a2efa8e034bbfb4
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-07-09 11:07:50 -0300

    up

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,13 @@
 module.exports = {
-	dialect: 'postgres',
-	host: 'containers-us-west-67.railway.app',
-	username: 'postgres',
-	password: 'BjPSOwaEpsqtTWrYw6HG',
-	database: 'railway',
-	port:5768,
-	define: {
-		timespamps: true,
-		underscored: true,
-		underscoredAll: true,
-	},
-};
+  dialect: 'postgres',
+  host: 'containers-us-west-67.railway.app',
+  username: 'postgres',
+  password: 'BjPSOwaEpsqtTWrYw6HG',
+  database: 'railway',
+  port: 5768,
+  define: {
+    timespamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+}
