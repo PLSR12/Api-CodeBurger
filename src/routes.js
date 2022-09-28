9a921bc7b68fd1507705a877a1059cfa0618fd0b
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-09-28 16:26:42 -0300

    [*] update routes

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,11 @@ import multer from 'multer'
 
 import multerConfig from './config/multer'
 
-import SessionController from './app/controllers/SessionController'
-import UserController from './app/controllers/UserController'
-import ProductController from './app/controllers/ProductController'
 import CategoryController from './app/controllers/CategoryController'
 import OrderController from './app/controllers/OrderController'
+import ProductController from './app/controllers/ProductController'
+import SessionController from './app/controllers/SessionController'
+import UserController from './app/controllers/UserController'
 
 import authMiddleware from './app/middlewares/auth'
 
@@ -23,12 +23,14 @@ routes.use(authMiddleware)
 
 routes.post('/products', upload.single('file'), ProductController.store)
 routes.get('/products', ProductController.index)
+routes.get('/products/:id', ProductController.show)
 routes.put('/products/:id', upload.single('file'), ProductController.update)
+routes.delete('/products/:id', ProductController.delete)
 
 routes.post('/categories', upload.single('file'), CategoryController.store)
 routes.get('/categories', CategoryController.index)
 routes.put('/categories/:id', upload.single('file'), CategoryController.update)
-
+routes.delete('/categories/:id', CategoryController.delete)
 
 routes.post('/orders', OrderController.store)
 routes.put('/orders/:id', OrderController.update)
